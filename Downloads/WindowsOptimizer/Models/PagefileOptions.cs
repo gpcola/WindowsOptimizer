09c49437dfc6485ae2be55bebda20fdbfcc4d8c5
TheTekTeam <gpcola@gmail.com>
2026-04-18 18:41:27 +0100

    Harden Windows publish pipeline and release packaging

--- Downloads/WindowsOptimizer/Models/PagefileOptions.cs
+++
@@ -1,9 +0,0 @@
-namespace WindowsOptimizer.Models
-{
-    public sealed class PagefileOptions
-    {
-        public string DriveLetter { get; set; } = "D";
-        public int InitialSizeMb { get; set; } = 2048;
-        public int MaximumSizeMb { get; set; } = 4096;
-    }
-}

fc3fe1e820711cdaf667c8f4bba07a41db6df01d
Gianpiero Colagiacomo <gpcola@gmail.com>
2026-05-17 22:01:57 +0100

    Show branded splash before main window

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,5 +4,18 @@ namespace WindowsOptimizer
 {
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            MainWindow = mainWindow;
+
+            var splash = new SplashWindow
+            {
+                Owner = mainWindow
+            };
+
+            splash.ShowDialog();
+            mainWindow.Show();
+        }
     }
 }

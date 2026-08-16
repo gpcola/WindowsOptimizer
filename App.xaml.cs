e69ef4119f11e8838c64cd43550294c4574b179c
Gianpiero Colagiacomo <gpcola@gmail.com>
2026-08-16 16:39:53 +0100

    Start main window with quick mode enabled by default

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,29 @@
+using System;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace WindowsOptimizer
 {
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var mainWindow = new MainWindow();
+            MainWindow = mainWindow;
+
+            EventHandler? contentRendered = null;
+            contentRendered = (_, _) =>
+            {
+                mainWindow.ContentRendered -= contentRendered;
+                mainWindow.Dispatcher.BeginInvoke(
+                    DispatcherPriority.ContextIdle,
+                    new Action(mainWindow.EnableQuickModeShell));
+            };
+
+            mainWindow.ContentRendered += contentRendered;
+            mainWindow.Show();
+        }
     }
 }

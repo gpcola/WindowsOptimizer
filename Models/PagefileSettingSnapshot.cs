9d33a790c066d4703655809ef2fd3d817496b99c
Gianpiero Colagiacomo <gpcola@gmail.com>
2026-08-16 20:38:01 +0100

    Remove obsolete pagefile snapshot model

--- Models/PagefileSettingSnapshot.cs
+++
@@ -1,9 +0,0 @@
-namespace WindowsOptimizer.Models
-{
-    public sealed class PagefileSettingSnapshot
-    {
-        public string Name { get; set; } = string.Empty;
-        public int InitialSize { get; set; }
-        public int MaximumSize { get; set; }
-    }
-}
